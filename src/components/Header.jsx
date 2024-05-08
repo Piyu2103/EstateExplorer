82061e25b49d365bd357f57db5b658fa2e8441db
Piyu2103 <singhalpiyush21@gmail.com>
2024-05-08 21:55:29 +0900

    Added the UI of SignIn,SignUp,ForgotPassword and OAuth

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,13 +14,13 @@ export default function Header() {
     <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
         <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
             <div>
-                <img src={Logo} alt="logo" className='h-10 cursor-pointer' width={200}onClick={()=>navigate("/")}></img>
+                <img src={Logo} alt="logo" className='h-10 cursor-pointer' width={200} onClick={()=>navigate("/")}></img>
             </div>
             <div>
                 <ul className='flex space-x-10'>
-                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/") && "text-black border-b-red-500"}`} onClick={()=>navigate("/")}>Home</li>
-                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/offers") && "text-black border-b-red-500" }`} onClick={()=>navigate("/offers")}>Offers</li>
-                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in") && "text-black border-b-red-500" }`} onClick={()=>navigate("/sign-in")}>Sign in</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMathRoute("/") ? "text-black border-b-red-500":"text-gray-400 border-b-transparent"}`} onClick={()=>navigate("/")}>Home</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMathRoute("/offers") ? "text-black border-b-red-500":"text-gray-400 border-b-transparent"}`} onClick={()=>navigate("/offers")}>Offers</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMathRoute("/sign-in") ? "text-black border-b-red-500":"text-gray-400 border-b-transparent"}`} onClick={()=>navigate("/sign-in")}>Sign in</li>
                 </ul>
             </div>
         </header>
--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,7 +1,61 @@
-import React from 'react'
-
-export default function ForgotPassword() {
+import React, { useState } from "react";
+import Key from "../images/key.jpg";
+import { Link } from "react-router-dom";
+import OAuth from "../components/OAuth";
+export default function SignIn() {
+  const [email, setEmail] = useState("");
+  function onChange(e) {
+    setEmail(e?.target?.value)
+  }
   return (
-    <div>ForgotPassword</div>
-  )
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">Forgot Password</h1>
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+        <div className="md:w-[67%] md: mb-6 lg:w-[50%] lg:mb-12">
+          <img src={Key} alt="Key" className="w-full rounded-2xl" />
+        </div>
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form>
+            <input
+              className="w-full px-4 py-2 mb-6 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+              type="email"
+              id="email"
+              value={email}
+              placeholder="Email Address"
+              onChange={onChange}
+            />
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
+              <p>
+                Don't have a account?
+                <Link
+                  to="/sign-up"
+                  className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
+                >
+                  Register
+                </Link>
+              </p>
+              <p>
+                <Link
+                  to="/sign-in"
+                  className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out"
+                >
+                  Sign in instead
+                </Link>
+              </p>
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+            >
+              Send Reset Password
+            </button>
+            <div className="my-4 flex items-center before:flex-1 before:border-t before:border-gray-300  after:flex-1 after:border-t after:border-gray-300">
+              <p className="text-center font-semibold mx-4">OR</p>
+            </div>
+            <OAuth/>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
